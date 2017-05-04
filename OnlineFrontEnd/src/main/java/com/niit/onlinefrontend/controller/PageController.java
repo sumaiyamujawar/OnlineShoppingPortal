c2f53e9d6218cb074dacf90abfe52f5f3223af0c
dell <dell@dellpc>
2017-05-05 00:09:43 +0530

    Created Dispatcher servlet, web.xml, page controller, home page.

---
+++ OnlineFrontEnd/src/main/java/com/niit/onlinefrontend/controller/PageController.java
@@ -0,0 +1,19 @@
+package com.niit.onlinefrontend.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PageController {
+
+	@RequestMapping(value={"/", "/home","/index"})
+	
+	public ModelAndView index(){
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("greeting", "Welcome to Online Shopping Portal ");
+		return mv;
+	}
+	
+	
+}
